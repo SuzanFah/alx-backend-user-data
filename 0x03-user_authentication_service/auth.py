b1436fed7daeb604b6ed15cfe442ec9ffa7d6850
SuzanFah <susiefahim@gmail.com>
2024-11-20 05:44:12 +0200

    codecode

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -60,7 +60,10 @@ class Auth:
 
     def destroy_session(self, user_id: int) -> None:
         """Destroys a session for the user"""
-        self._db.update_user(user_id, session_id=None)
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except NoResultFound:
+            return None
 
 
 def _hash_password(password: str) -> bytes:
