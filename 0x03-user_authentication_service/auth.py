7d1c14b555330e171863ed635b89a2ad750403f8
SuzanFah <susiefahim@gmail.com>
2024-11-20 05:39:38 +0200

    bbbbbbbbbbbbbb

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ from user import User
 from sqlalchemy.orm.exc import NoResultFound
 import bcrypt
 import uuid
+from typing import Union
 
 
 def _hash_password(password: str) -> bytes:
@@ -51,18 +52,18 @@ class Auth:
         except NoResultFound:
             return None
 
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """Get user from session ID"""
+        if session_id is None:
+            return None
+        
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
+
 
 def _generate_uuid() -> str:
     """Generate UUID"""
     return str(uuid.uuid4())
-
-def get_user_from_session_id(self, session_id: str) -> User:
-    """Returns user based on session ID"""
-    if session_id is None:
-        return None
-    
-    try:
-        user = self._db.find_user_by(session_id=session_id)
-        return user
-    except NoResultFound:
-        return None
